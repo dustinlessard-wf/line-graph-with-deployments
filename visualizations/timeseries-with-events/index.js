a5590ba539dd113cb5ab9a03decedd156a726f9c
Dustin Pauze <dustinpauze@Dustins-iMac.lan>
2021-07-10 10:07:51 -0400

    wip

--- visualizations/timeseries-with-events/index.js
+++ visualizations/timeseries-with-events/index.js
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types';
 import {
     Line,
     LineChart,
-    CartesianGrid,
     Legend,
     XAxis,
     YAxis,
-    ReferenceArea,
-    ReferenceLine,
-    Tooltip
+    ReferenceArea, 
+    ReferenceLine
 } from 'recharts';
-import { Card, CardBody, HeadingText, NrqlQuery, Spinner, AutoSizer } from 'nr1';
+import { Card, CardBody, CartesianGrid, HeadingText, NrqlQuery, Spinner, AutoSizer } from 'nr1';
 import { DateTime } from 'luxon';
 const createTrend = require('trendline');
 
@@ -39,8 +37,6 @@ export default class LineGraphWithDeploymentsVisualization extends React.Compone
 
 
     transformData = (rawData) => {
-        console.log('rawData:');
-        console.log(rawData);
         let list = [];
         rawData[0].data.forEach(item => {
             list.push({ x: item.x, y: item.y });
@@ -60,8 +56,7 @@ export default class LineGraphWithDeploymentsVisualization extends React.Compone
     getTimeSeriesLines = (rawData) => {
         let list = [];
         rawData.forEach(item => {
-            console.log(item.metadata.color);
-            list.push(<Line name={item.metadata.name} data={this.getBasicPoints(item.data)} type="linear" dataKey="y" stroke={item.metadata.color} strokeWidth={2} dot={false} />);
+            list.push(<Line name={item.metadata.name} data={this.getBasicPoints(item.data)} type="monotone" dataKey="y" stroke={item.metadata.color} strokeWidth={1} dot={false} />);
         })
         return list;
     }
@@ -74,7 +69,7 @@ export default class LineGraphWithDeploymentsVisualization extends React.Compone
     };
 
     render() {
-        const { accountId, eventQuery, eventColor, showEvents, showTrendLine, timeseriesQuery, timeseriesColor } = this.props;
+        const { accountId, areasQuery, eventQuery, eventColor, numberOfTicks, showEvents, showTrendLine, timeseriesQuery, timeseriesColor } = this.props;
         let events;
         // const nrqlQueryPropsAvailable =
         // eventQuery &&
@@ -103,9 +98,6 @@ export default class LineGraphWithDeploymentsVisualization extends React.Compone
                             if (data != null) {
                                 timeSerieslines = this.getTimeSeriesLines(data);
                                 transformedData = this.transformData(data);
-                                
-                                console.log('timeSerieslines');
-                                console.log(timeSerieslines);
                             }
 
                             return (
@@ -141,32 +133,44 @@ export default class LineGraphWithDeploymentsVisualization extends React.Compone
                                             console.log('xMax');
                                             console.log(xMin);
 
-                                            let lines = [];
+                                            let events = [];
+                                            let areasEvents = [];
                                             let trendData = [];
                                             var trendLine;
 
                                             if (showEvents == true) {
-                                                console.log('showEvents is true');
                                                 data != null ? data[0]?.data?.forEach(item => {
                                                     // console.log('adding new event');
                                                     // console.log(item.x);
-                                                    lines.push(<ReferenceLine fillOpacity="0.1" x={item.x} stroke={eventColor || "grey"} strokeWidth="1" label={{ position: 'left', angle: 90, value: item.name, fill: eventColor || 'grey', fontSize: 10 }} />);
+                                                    events.push(<ReferenceLine fillOpacity="0.1" x={item.x} stroke={eventColor || "grey"} strokeWidth="1" label={{ position: 'left', angle: 90, value: item.name, fill: eventColor || 'grey', fontSize: 8 }} />);
 
                                                 }) : ''
                                             }
 
                                             if (showTrendLine === true && transformedData.length !== 0) {
-                                                console.log('show trend line');
-
                                                 let trend = createTrend(transformedData, 'x', 'y');
-                                                console.log('trend:');
-                                                console.log(trend);
                                                 xValues.forEach((xValue) => {
                                                     trendData.push({ y: trend.calcY(xValue), x: xValue })
                                                 });
                                                 trendLine = <Line name="trend" data={trendData} type="linear" dataKey="y" stroke="red" dot={false} />;
                                             }
+                                            
+                                            return (
+                                                <NrqlQuery
+                                    query={areasQuery}
+                                    accountId={parseInt(accountId)}
+                                    pollInterval={NrqlQuery.AUTO_POLL_INTERVAL}
+                                >
+                                    {({ data, loading, error }) => {
+                                            console.log('areasQuery data');
+                                            console.log(data);
+                                            data != null ? data[0]?.data?.forEach(item => {
+                                                // console.log('adding new event');
+                                                // console.log(item.x);
+                                                areasEvents.push(<ReferenceArea x1={item.start} x2={item.end} stroke="red" fill="pink" strokeOpacity={0.3} label={item.name} />);
 
+                                            }) : ''
+                                            console.log(areasEvents);
                                             return (
                                                 <LineChart
                                                     width={width}
@@ -177,13 +181,18 @@ export default class LineGraphWithDeploymentsVisualization extends React.Compone
                                                         left: 20,
                                                         bottom: 5,
                                                     }}>
-                                                    <Legend verticalAlign="top" height={36}/>
-                                                    <XAxis dataKey="x" type="number" tickCount="10" tick={{ fill: 'grey', fontSize: 8 }} tickFormatter={this.formatTick} domain={['auto', 'auto']} />
-                                                    <YAxis dataKey="y" domain={[yMin, yMax]} />
+<Legend/>
+                                                    <XAxis dataKey="x" type="number" tickCount={numberOfTicks} tick={{ fill: 'grey', fontSize: 8 }}  domain={['auto', 'auto']} />
+                                                    <YAxis domain={['auto', 'auto']} />
+                                                    <CartesianGrid/>
+                                                    {events}
+                                                    {areasEvents}
                                                     {trendLine}
                                                     {timeSerieslines}
-                                                    {lines}
+                        
                                                 </LineChart>)
+                                    }}
+                                                </NrqlQuery>)
                                         }
                                     }}
                                 </NrqlQuery>)
@@ -213,9 +222,14 @@ const EmptyState = () => (
             <code>
                 SELECT percentage(count(*), WHERE result = 'SUCCESS') *100 FROM SyntheticCheck WHERE custom.Solution IN ('Financial Reporting', 'Home', 'Integrated Risk', 'Operational Reporting', 'XBRL Financial Reporting') AND custom.Domain ='app.wdesk.com' FACET custom.Solution TIMESERIES LIMIT MAX SINCE 1 week ago
             </code>
-            <br/><br/>
+            <br /><br />
             <code>
-                SELECT timestamp, deployment.release.name as 'name', deployment.service FROM SoftwareDevelopmentLifecycle WHERE event = 'deploy_complete' AND deployment.environment = 'prod' LIMIT MAX SINCE 1 week ago
+                SELECT timestamp, deployment.release.name as 'name', deployment.service FROM SoftwareDevelopmentLifecycle WHERE event = 'deploy_complete' AND deployment.environment = 'prod' AND deployment.category = 'prod' AND deployment.type != 'ApiGatewayDeploy' AND deployment.repository.taxonomy_group != 'Infrastructure' AND deployment.release.name IS NOT NULL AND deployment.release.name like 'c%' OR deployment.release.name like '%d' LIMIT MAX SINCE 1 week ago
+            </code>
+            <br />
+            <br />
+            <code>
+                FROM OI SELECT ticket as 'name', detect_start as 'start', restore_end as 'end' WHERE detect_start IS NOT NULL
             </code>
         </CardBody>
     </Card>
